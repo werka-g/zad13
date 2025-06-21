7145a433865ecfc181c3fc81515a0b0d245ffd5f
werka-g <weronikagin@gmail.com>
2025-06-21 23:55:46 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,5 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
-import { glob } from 'node:fs/promises'
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/zad13/',
 });
-
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-const inputs = [];
-
-for await (const entry of glob('src/**/*.html')) {
-  console.log(resolve(__dirname, entry));
-  inputs.push(resolve(__dirname, entry));
-}
-
-export default defineConfig({
-  plugins: [],
-
-  root: resolve(__dirname, 'src'),
-  build: {
-    emptyOutDir: true,
-    rollupOptions: {
-      input: inputs,
-    },
-    outDir: resolve(__dirname, 'dist'),
-  },
-})
