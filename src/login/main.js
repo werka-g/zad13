42c6e78a6308ad4837637b41f6311dffc5a5c047
werka-g <weronikagin@gmail.com>
2025-06-21 23:34:19 +0200

    Update main.js

--- src/login/main.js
+++ src/login/main.js
@@ -1,1 +1,13 @@
+const { data: { user } } = await supabase.auth.getUser();
+const isLoggedIn = !!user;
+
 console.log('nested');
+
+if (isLoggedIn) {
+  document.getElementById('logout-btn').style.display = 'block';
+}
+
+document.getElementById('logout-btn').addEventListener('click', async () => {
+  await supabase.auth.signOut();
+  window.location.reload();
+});
